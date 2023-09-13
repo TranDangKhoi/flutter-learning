3b9b7811b890f10d1155f7b412672f904dc1baf0
TranDangKhoi <baits16@gmail.com>
2023-09-13 23:43:07 +0700

    :+1: StatelessWidget and other widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(body: Text("Hello world!")),
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.red[900], title: const Text("Hello World")),
+        body: const Center(
+          // child: const Text("Hello world!", textAlign: TextAlign.center),
+          child: MyOwnWidget(loading: false),
+        ),
+      ),
+    ),
+    debugShowCheckedModeBanner: false,
   ));
+}
+
+class MyOwnWidget extends StatelessWidget {
+  final bool loading;
+
+  const MyOwnWidget({super.key, required this.loading});
+  @override
+  Widget build(BuildContext context) {
+    return loading ? const CircularProgressIndicator() : const Text("State");
+  }
 }
